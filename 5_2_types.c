9046eb422da22293a2af73058c6047e2f4480aae
AR <aarokan@gmail.com>
2023-10-18 23:26:15 +0300

    Boolean expression

---
+++ 5_2_types.c
@@ -0,0 +1,21 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <stdbool.h> // Imported the header file to use bool type
+
+int main(void)
+{
+// bool, a Boolean expression of either true or false
+bool isCLangFun = true;
+
+// char, a single character like a or 2
+// double, a floating-point value with more digits than a float
+// float, a floating-point value, or real number with a decimal value
+// int, integers up to a certain size, or number of bits
+// long, integers with more bits, so they can count higher than an int
+// string, a string of characters
+
+printf("%d \n", isCLangFun);
+}
+
+// %c   %f   %i   %li   %s
+

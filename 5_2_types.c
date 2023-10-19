e95971e2ba6073e9d6ed4a9fb626b03368e22582
AR <aarokan@gmail.com>
2023-10-19 23:46:04 +0300

    double type

--- 5_2_types.c
+++ 5_2_types.c
@@ -4,26 +4,33 @@
 
 int main(void)
 {
-// bool, a Boolean expression of either true or false
-bool isCLangFun = true;
+    // bool, a Boolean expression of either true or false
+    bool isCLangFun = true;
 
-// int, integers up to a certain size, or number of bits
-int myNum = 2;
+    // int, integers up to a certain size, or number of bits
+    int myNum = 2;
 
-// float, a floating-point value, or real number with a decimal value
-float myFloatNum = 4.99;
+    // float, a floating-point value, or real number with a decimal value
+    float myFloatNum = 4.99;
+    float floatPiLimit = 3.141592653589793;
 
-// char, a single character like a or 2
-char myLetter = 'd';
+    // char, a single character like a or 2
+    char myLetter = 'd';
 
-// double, a floating-point value with more digits than a float
-// long, integers with more bits, so they can count higher than an int
-// string, a string of characters
+    // double, a floating-point value with more digits than a float
+    double pi = 3.141592653589793;
+    double doubleLimit = 9876543210.123456789012345;
 
-printf("%d \n", isCLangFun);
-printf("%i \n", myNum);
-printf("%f \n", myFloatNum);
-printf("%c \n", myLetter);
+    // long, integers with more bits, so they can count higher than an int
+    // string, a string of characters
+
+    printf("%d\n", isCLangFun);
+    printf("%i\n", myNum);
+    printf("%f\n", myFloatNum);
+    printf("%f\n", floatPiLimit);
+    printf("%c\n", myLetter);
+    printf("%lf\n", pi);
+    printf("%lf\n", doubleLimit);
 }
 
 // %c   %f   %i   %li   %s
